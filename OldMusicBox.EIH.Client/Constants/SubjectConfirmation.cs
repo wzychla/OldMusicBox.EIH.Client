422b9111da377880fada667416af7a0bab4f8075
Wiktor Zychla <wzychla@gmail.com>
2021-03-25 13:37:33 +0100

    missing files

---
+++ OldMusicBox.EIH.Client/Constants/SubjectConfirmation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldMusicBox.EIH.Client.Constants
+{
+    /// <summary>
+    /// Subject confirmation
+    /// </summary>
+    public class SubjectConfirmation
+    {
+        public const string BEARER = "urn:oasis:names:tc:SAML:2.0:cm:bearer";
+    }
+}
